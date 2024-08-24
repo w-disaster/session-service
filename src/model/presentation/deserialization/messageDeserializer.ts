dce304b0e64f2c35d4bc37f0a61bbc81f2d6e2c1
Luca Fabri <luca.fabri1999@gmail.com>
2024-08-24 19:30:14 +0200

    fix: delete json parse because of nested serialization

--- src/model/presentation/deserialization/messageDeserializer.ts
+++ src/model/presentation/deserialization/messageDeserializer.ts
@@ -1,4 +1,4 @@
-import { TextMessage } from '../../message'
+import { NotificationMessage, TextMessage } from '../../message'
 import { AbstractDeserializer } from './deserializer'
 import { UserDeserializer } from './userDeserializer'
 
@@ -8,6 +8,17 @@ export class TextMessageDeserializer extends AbstractDeserializer<TextMessage> {
   }
 
   deserializeJson(json: any): TextMessage {
+    console.log('D', json)
     return new TextMessage(new UserDeserializer().deserialize(json.sender), json.content)
+  }
+}
+
+export class NotificationMessageDeserializer extends AbstractDeserializer<NotificationMessage> {
+  isJsonValid(json: any): boolean {
+    return typeof json.sender === 'object' && typeof json.content === 'number'
+  }
+
+  deserializeJson(json: any): NotificationMessage {
+    return new NotificationMessage(new UserDeserializer().deserialize(json.sender), json.content)
   }
 }
