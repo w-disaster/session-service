adb733213e5858b2bf76c0fdfe0c7cc04d00e19e
Luca Fabri <luca.fabri1999@gmail.com>
2024-10-09 15:49:21 +0200

    chore: add video state deserializer

---
+++ src/application/presentation/deserialization/videoStateDeserializer.ts
@@ -0,0 +1,22 @@
+import { PlayState, VideoState } from '../../room/video'
+import { AbstractDeserializer } from './deserializer'
+
+class PlayStateDeserializer extends AbstractDeserializer<PlayState> {
+  isJsonValid(json: any): boolean {
+    return (Object.values(PlayState) as string[]).includes(json)
+  }
+
+  deserializeJson(json: any): PlayState {
+    return json as PlayState
+  }
+}
+
+export class VideoStateDeserializer extends AbstractDeserializer<VideoState> {
+  isJsonValid(json: any): boolean {
+    return typeof json.state === 'string' && typeof json.timestamp === 'number'
+  }
+
+  deserializeJson(json: any): VideoState {
+    return { state: new PlayStateDeserializer().deserialize(json.state), timestamp: json.timestamp }
+  }
+}
